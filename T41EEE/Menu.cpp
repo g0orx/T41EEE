b33e39fb18921fe603170f079dec4412c37c8823
John Melton G0ORX <john.d.melton@googlemail.com>
2024-01-14 10:43:11 +0000

    Removed Serial.println

--- T41EEE/Menu.cpp
+++ T41EEE/Menu.cpp
@@ -26,7 +26,6 @@ int (*functionPtr[])() = { &BearingMaps, &CWOptions, &RFOptions, &VFOSelect,
 *****/
 void ShowMenu(const char *menu[], int where)
 {
-  Serial.println(String(__FUNCTION__));
   tft.setFontScale( (enum RA8875tsize) 1);  
 
   if (menuStatus == NO_MENUS_ACTIVE)                                        // No menu selected??
